1d3ebd44e230e8b51fe295c6ee1b18520b39c4ad
ranakhalil <rana.khalilllllll@gmail.com>
2026-09-08 09:41:50 +0300

    Add files via upload

---
+++ telegram_bot.py
@@ -0,0 +1,22 @@
+# 1) Creating a telegram bot using BotFather
+# 2) Getting our telegram bot token
+# 3) Getting our chat id
+# 4) Sending message using Python
+
+# make sure to /start your bot to work
+
+TOKEN = "8893150300:AAHoDwaqYHTwOJusgo-9TeXY2HXBd1Dwa0Q"
+CHAT_ID = '5949246928'
+
+import requests
+
+# Uncomment to get updates
+# url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+# print(requests.get(url).json())
+
+def send_alert():
+    message = 'SERIOUS ALERT! CHECK ON YOUR BABY RIGHT NOW!'
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+
+    r = requests.get(url)
+    print(r.json())
