567ed961abed759db056e23cc90e81dad36d0725
ranakhalil <rana.khalilllllll@gmail.com>
2026-09-11 07:47:38 +0300

    Update serialpython.py

--- serialpython.py
+++ serialpython.py
@@ -1,17 +1,19 @@
 # pip install pyserial
 import serial
 
-port = 'COM3'
-baudrate = 9600
-
-ser = serial.Serial(port, baudrate)
+ser = serial.Serial('COM3', 9600)
 
 # Parse one line of data like: "Temp:26.30,Gas:180.00,Awake:1,Fan:1,Light:0"
+# string manipulation
 def parse_line(line):
     data = {}
     pairs = line.split(',')
     for pair in pairs:
         if ':' in pair:
             key, value = pair.split(':')
-            data[key.strip()] = value.strip()
-    return data
+            data[key.strip()] = value.strip() # Removes extra spaces
+    return data # The values are strings
+
+def cry(input):
+    input= input+ '\r'
+    ser.write(input.encode())
