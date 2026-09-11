606a69d47d503e1775a4863f44aee58b2497b731
ranakhalil <rana.khalilllllll@gmail.com>
2026-09-11 07:44:41 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+#####################PRIORITIES NEEDS HANDLING ASAP#####################
 from tkinter import *
 import numpy as np
 # import random
@@ -10,15 +11,80 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from serialpython import *
 from trigger_alert import *
-# from video_player import play
-# from ml_classification import predict_cry
+from video_player import play
+
+####################################
+#Model's Training Libraries
+import pandas as pd
+import pickle
+
+#Audio Processing Libraries (Sarah)
+import os
+import librosa
+import noisereduce as nr
+import warnings
+warnings.filterwarnings("ignore")
+#load outside to avoid being loaded multiple times
+with open("already_trained_model.pkl", "rb") as file:
+    saved_model = pickle.load(file)
+#The label map
+label_map = {0:"hungry" , 1:"discomfort", 2:"tired"}
+#Essential Functions in the secondary file
+def extract_cry_features(signal, sr=16000):
+    cleaned_signal = nr.reduce_noise(y=signal, sr=SAMPLE_RATE)
+    intervals=librosa.effects.split(cleaned_signal,top_db=20)
+    if len(intervals)>0:
+        VAD_signal=np.concatenate([cleaned_signal[start:end]for start,end in intervals])
+    else:
+        VAD_signal=cleaned_signal
+    if len(VAD_signal) == 0:
+            return None
+    MFCCs=librosa.feature.mfcc(y=VAD_signal,sr=sr,n_mfcc=13)
+    MFCC_mean=np.mean(MFCCs,axis=1)
+    MFCC_std=np.std(MFCCs,axis=1)
+    f0=librosa.yin(VAD_signal,fmin=librosa.note_to_hz('C3'),fmax=librosa.note_to_hz('C7'),frame_length=2048,sr=sr)
+    f0_clean=f0[~np.isnan(f0)]if f0 is not None else[]
+    f0_mean=np.mean(f0_clean)if len(f0_clean)>0 else 0.0
+    f0_std=np.std(f0_clean) if len (f0_clean)>0 else 0.0
+    rms = librosa.feature.rms(y=VAD_signal)
+    intensity_mean = np.mean(rms)
+    intensity_std = np.std(rms)
+    duration = librosa.get_duration(y=VAD_signal, sr=sr)
+    feature_vector=np.hstack([
+        MFCC_mean,
+        MFCC_std,
+        f0_mean,
+        f0_std,
+        intensity_mean,
+        intensity_std,
+        duration])
+    return feature_vector
+# Getting Live Records <<<@ Rana GUI
+def get_live_features(audio, sr=16000):
+    global baby_state
+    live_signal = audio.astype(np.float32) / 32768.0
+    if np.max(np.abs(live_signal)) < 0.01:
+        # print("Silence detected (No sound)")
+        cry('0')
+        return None
+    cry('1')
+    return extract_cry_features(live_signal, sr=sr)
+
+#Live Recording Classifying>>> @rana GUI
+def classify_live_record(x_live):
+    Prediction= saved_model.predict(x_live)
+    return str(label_map[Prediction.item()])
+####################################
 
 gas_value = 0
+baby_state= ""
+video_window= None
+alert_window= None
 
 MAX_AMPLITUDE = 4000
 
 CHUNK_SIZE = 1024
-SAMPLE_RATE = 44100
+SAMPLE_RATE = 16000
 BAR_COUNT = 80
 
 def create_waveform(window):
@@ -81,23 +147,31 @@ def create_waveform(window):
 
     def read_stream():
         nonlocal levels
+        global baby_state
 
         data = stream.read(
             CHUNK_SIZE,
             exception_on_overflow= False
         )
 
-        # predict_cry(data)
-
         audio_array = np.frombuffer(
             data,
             dtype= np.int16 # tells NumPy how to interpret the raw bytes that PyAudio returned.
         )
 
+        ####################################
+        # bytes has no attribute 'flatten'
+        live_feature_vector = get_live_features(audio_array)
+        if live_feature_vector is not None:
+            X_live = live_feature_vector.reshape(1, -1)  # this line was added by Dhuha
+            # print("Live Feature Vector Ready! Shape:", X_live.shape)
+            # calling pretrained classifying model
+            baby_state= classify_live_record(X_live)
+        ####################################
+
         amplitude = float(
             np.mean(np.abs(audio_array))
         )
-
         levels.pop(0)
         levels.append(amplitude)
 
@@ -117,16 +191,50 @@ def create_waveform(window):
     read_stream()
 
     return close_waveform
+############ suggestion ############
+def update_suggestion(window, SUGGESTIONS_label):
+    global baby_state
+    if baby_state == "hungry":
+        SUGGESTIONS_label.config(text=f"SUGGESTION: Consider feeding your baby.", fg="green")
+    if baby_state == "tired":
+        SUGGESTIONS_label.config(text=f"SUGGESTION: URGENT! Your baby requires your care right now.", fg="red")
+    if baby_state == "discomfort":
+        SUGGESTIONS_label.config(text=f"SUGGESTION: Consider checking diaper, clothing, position.", fg="green")
+    if baby_state == "fine":
+        SUGGESTIONS_label.config(text=f"SUGGESTION: NO suggestions.", fg="green")
+
+    window.after(23, update_suggestion, window, SUGGESTIONS_label)
+############ baby_state ############
+def update_state(window, babystate_label):
+    global baby_state
+    global video_window
+    babystate_label.config(text=f"YOUR BABY IS {baby_state}")
+    if baby_state == "hungry":
+        if video_window is None:
+            video_window= play(window)
+    else:
+        if video_window is not None:
+            video_window.destroy()
+            video_window = None
+    if baby_state == "tired":
+        ser.write("B1\r".encode())
+    else: ser.write("B0\r".encode())
+    if baby_state == "discomfort" or baby_state == "tired" or baby_state == "fine":
+        if video_window is not None:
+            video_window.destroy()
+            video_window = None
+
+    window.after(23, update_state, window, babystate_label)
 
 ############ serial ############
 def update_gui(window, temp_label, gas_label, awake_label, fan_label, light_label):
     global gas_value
+    global alert_window
     if ser.in_waiting > 0:  # only read if there's actually new data waiting
         raw_line = ser.readline().decode('utf-8', errors='ignore').strip()
         # serial device sends bytes -> ser.readline() -> decode bytes into text -> remove newline characters
         if raw_line:
             data = parse_line(raw_line)
-
             if "Temp" in data:
                 temp_label.config(text=f"Temp: {data['Temp']} °C")
             if "Gas" in data:
@@ -142,7 +250,14 @@ def update_gui(window, temp_label, gas_label, awake_label, fan_label, light_labe
                 light_text = "ON" if data['Light'] == "1" else "OFF"
                 light_label.config(text=f"Light: {light_text}")
 
+        if gas_value > 500 and alert_window is None:
+            alert_window= trigger_alert(window)
+        elif gas_value <= 500 and alert_window is not None:
+            alert_window.destroy()
+            alert_window = None
+
     window.after(23, update_gui, window, temp_label, gas_label, awake_label, fan_label, light_label)
+
 
 def main():
     window = Tk()
@@ -159,12 +274,12 @@ def main():
     time_label.pack(
         pady= 2
     )
-    def update():
+    def update_time():
         time_string = strftime("%H:%M:%S %p")
         time_label.config(text=time_string)
 
-        window.after(1000, update)
-    update()
+        window.after(1000, update_time)
+    update_time()
     ############ voice-memo waveform ############
     wave_label = Label(window,
                        text="LIVE AUDIO PROCESSING",
@@ -251,15 +366,12 @@ def main():
         side=TOP,
         pady= 2
     )
-    ############ baby_state(randomized for now) ############
+    ############ baby_state ############
     # LIST= ('HUNGRY','UNCOMFORTABLE','TIRED','FINE')
     # baby_state= random.choice(LIST)
-    baby_state = 'UNCOMFORTABLE'
-    # needs fixing
     # if baby_state == 'HUNGRY' and gas_value <= 500:
     #    play()
     # connect with serial
-    # UNCOMFORTABLE -> suggest checking diaper, clothing, position.
 
     babystate_label = Label(window,
                             text=f"YOUR BABY IS {baby_state}.",
@@ -271,10 +383,8 @@ def main():
         side=TOP,
         pady= 2
     )
-    # UPDATE THIS TWO REGULARLY
-    # UPDATE_GUI_STATE
-    # IF BABY GETS FLAGGED AS AWAKE ONCE, DOES IT EVER GET FLAGGED AS ASLEEP AGAIN??
-    ############
+    update_state(window, babystate_label)
+    ############ suggestions ############
     # Laptop to Microcontroller
     # Cry detected / Cry ended
     # Laptop to Microcontroller
@@ -282,19 +392,25 @@ def main():
     # Laptop to Microcontroller
     # Buzzer on (tired cry alert)
     SUGGESTIONS_label = Label(window,
-                            text=f"SUGGESTION: --",
-                            font=("ALGERIAN", 15, 'bold'),
-                            fg='#78d4ff',
-                            bg='white',
-                            )
+                              text=f"SUGGESTION: --",
+                              font=("ALGERIAN", 15, 'bold'),
+                              fg='green',
+                              bg='white',
+                              )
     SUGGESTIONS_label.pack(
         side=TOP,
         pady=2
     )
-    # MIGHT NEED A SEPARATE GUI
-    # Hungry cry: display message and play calming video clip.
-    # Tired cry: sound buzzer immediately and display warning(this pattern is urgent).
-    # Discomfort cry: display message and suggest checking diaper, clothing, position.
+    def dismiss_cry(babystate_label):
+        babystate_label.config(text="YOUR BABY IS FINE.")
+    dismiss_button = Button(window,
+                            text="DISMISS",
+                            command= dismiss_cry,
+                            )
+    dismiss_button.pack(
+        side=TOP,
+        pady=2
+    )
     ############ MAINLOOP ############
     window.mainloop()
 
